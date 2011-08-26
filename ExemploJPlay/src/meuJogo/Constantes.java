040a571a1310fef9073d3b0911c48032ba0f1efe
lapaesleme <lapaesleme@gmail.com>
2011-08-26 01:06:36 +0000

    Versão inicial.

---
+++ ExemploJPlay/src/meuJogo/Constantes.java
@@ -0,0 +1,16 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package meuJogo;
+
+/**
+ *
+ * @author Luiz Leme
+ */
+public class Constantes {
+
+  public static final int MENU = 0;
+  public static final int FASE1 = 1;
+  public static final double G = 0.0005;
+}
