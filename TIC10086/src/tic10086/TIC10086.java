dcea6c76fec4124f1806df9fb3e3cdfbc32e7d4e
Luiz Leme <lapaesleme@gmail.com>
2015-09-23 13:23:58 -0300

    ok

--- TIC10086/src/tic10086/TIC10086.java
+++ TIC10086/src/tic10086/TIC10086.java
@@ -1,9 +1,35 @@
 package tic10086;
 
+import org.apache.jena.query.Dataset;
+import org.apache.jena.query.ReadWrite;
+import org.apache.jena.rdf.model.Model;
+import org.apache.jena.rdf.model.Property;
+import org.apache.jena.rdf.model.RDFNode;
+import org.apache.jena.rdf.model.Resource;
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.rdf.model.StmtIterator;
+import org.apache.jena.tdb.TDBFactory;
+
 public class TIC10086 {
-    
+
     public static void main(String[] args) {
-        
+        String assemblerFile = "C:\\Users\\Luiz Leme\\Software\\apache-jena-fuseki-2.3.0\\run\\configuration\\tic10086.ttl";
+        Dataset dataset = TDBFactory.assembleDataset(assemblerFile);
+        dataset.begin(ReadWrite.READ);
+        Model model = dataset.getDefaultModel();
+        for (StmtIterator i = model.listStatements(); i.hasNext();) {
+            Statement s = i.nextStatement();
+            Resource subj = s.getSubject();
+            Property prop = s.getPredicate();
+            RDFNode obj = s.getObject();
+            
+            System.out.println(subj.getURI());
+            System.out.println(prop.getURI());
+            System.out.println(obj.toString());
+            System.out.println("");
+        }
+        dataset.end();
+
     }
-    
+
 }
---
+++ TIC10086/src/tic10086/Teste.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package tic10086;
+
+/**
+ *
+ * @author Luiz Leme
+ */
+public class Teste {
+    
+    public static void main(String[] args) {
+        int[] ls = {1,2,3};
+        for (int f: ls){
+            System.out.println(f);
+        }
+    }
+    
+}
