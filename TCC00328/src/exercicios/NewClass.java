319a6805a0c5ac4cb76d1e1ad1b20344bf0b2b7a
Luiz Leme <lapaesleme@gmail.com>
2015-12-05 18:23:29 -0200

    ok

--- TCC00328/src/exercicios/NewClass.java
+++ TCC00328/src/exercicios/NewClass.java
@@ -7,7 +7,6 @@ public class NewClass {
     public static void main(String[] args) {
         System.out.println("Helo world");
         System.out.println("Helo world");
-        System.out.println("Helo world");
     }
     
 }
