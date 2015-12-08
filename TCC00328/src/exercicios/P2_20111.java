4665980dd0de61c343ffdcda7b795e54399ada6f
Luiz Leme <lapaesleme@gmail.com>
2015-12-08 11:09:17 -0200

    ok

--- TCC00328/src/exercicios/P2_20111.java
+++ TCC00328/src/exercicios/P2_20111.java
@@ -8,14 +8,14 @@ public class P2_20111 {
 
     public static float determinante(float[][] matriz) {
         int i = 0;
-        float m = 0f;
+        float det = 0f;
         if (matriz[0].length == 1) {
             return matriz[0][0];
         } else {
             for (int j = 0; j <= matriz[0].length; j++) {
-                m += matriz[i][j] * Math.pow(-1, i + j) * determinante(cofator(matriz, i, j));
+                det += matriz[i][j] * Math.pow(-1, i + j) * determinante(cofator(matriz, i, j));
             }
-            return m;
+            return det;
         }
 
     }
